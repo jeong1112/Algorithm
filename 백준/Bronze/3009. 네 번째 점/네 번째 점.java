edcb9a40c04007196536fed8e345660e1d5ccf70
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2024-11-15 19:41:05 +0900

    [Bronze III] Title: 네 번째 점, Time: 124 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -1,43 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] p1 = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-        st = new StringTokenizer(br.readLine(), " ");
-        int[] p2 = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-        st = new StringTokenizer(br.readLine(), " ");
-        int[] p3 = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-        int resultX = 0;
-        int resultY = 0;
-        if(p1[0] == p2[0]){
+        String[] p1 = br.readLine().split(" ");
+        String[] p2 = br.readLine().split(" ");
+        String[] p3 = br.readLine().split(" ");
+        String resultX = "";
+        String resultY = "";
+        if(p1[0].equals(p2[0])){
             resultX = p3[0];
         }
-        else if(p1[0] == p3[0]){
+        else if( p1[0].equals(p3[0])){
             resultX = p2[0];
         }
-        else if(p2[0] == p3[0]){
+        else if( p2[0].equals(p3[0])){
             resultX = p1[0];
         }
 
-        if(p1[1] == p2[1]){
-            resultY = p3[1];
-        }
-
-        else if(p1[1] == p3[1]){
+        if(p1[1].equals(p3[1])){
             resultY = p2[1];
         }
-
-        else if(p2[1] == p3[1]){
+        else if( p1[1].equals(p2[1])){
+            resultY = p3[1];
+        }
+        else if( p2[1].equals(p3[1])){
             resultY = p1[1];
         }
+
+
 
         System.out.println(resultX + " " + resultY);
     }
