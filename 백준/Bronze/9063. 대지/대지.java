cd10fb65b49354643ec22a03fbed1a5c16cdbda6
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2024-11-17 19:33:51 +0900

    [Bronze III] Title: 대지, Time: 360 ms, Memory: 30004 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] X = new int[n];
+        int[] Y = new int[n];
         if(n <= 1){
             System.out.println(0);
             return;
         }
-        List<Integer> xValues = new ArrayList<>();
-        List<Integer> yValues = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            String s = br.readLine();
-            xValues.add(Integer.parseInt(s.split(" ")[0]));
-            yValues.add(Integer.parseInt(s.split(" ")[1]));
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            X[i] = Integer.parseInt(st.nextToken());
+            Y[i] = Integer.parseInt(st.nextToken());
         }
-        Collections.sort(xValues);
-        Collections.sort(yValues);
-        int resultX = xValues.get(xValues.size() - 1) - xValues.get(0);
-        int resultY = yValues.get(yValues.size() - 1) - yValues.get(0);
+        Arrays.sort(X);
+        Arrays.sort(Y);
+        int resultX = X[X.length - 1] - X[0];
+        int resultY = Y[Y.length - 1] - Y[0];
         System.out.println(resultX * resultY);
     }
 }
