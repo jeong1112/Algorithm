7b1284e8fe1e7067411f3a0fc10551d518ae302e
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-01-07 19:39:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1244 ms, Memory: 57508 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Position> positions = new ArrayList<Position>();
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            Position p = new Position(x, y);
+            positions.add(p);
+        }
+        Collections.sort(positions, new Comparator<Position>() {
+            @Override
+            public int compare(Position o1, Position o2) {
+                int res = 0;
+                if(o1.x > o2.x){
+                    res = 1;
+                }
+                else if(o1.x < o2.x){
+                    res = -1;
+                }
+                else{
+                    if(o1.y > o2.y){
+                        res = 1;
+                    }
+                    else if(o1.y < o2.y){
+                        res = -1;
+                    }
+                }
+                return res;
+            }
+        });
+        for (Position position : positions) {
+            System.out.println(position.x + " " + position.y);
+        }
+    }
+
+    public static class Position{
+        private int x;
+        private int y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
