5cf68fb49be57eeb600f45ada79f84a8e2a5f37a
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2025-01-14 17:13:38 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static boolean[] visited;
-    static int[][] arr;
+    static List[] computers ;
     static int count = 0;
     static int n;
     static int m;
@@ -14,28 +14,41 @@ public class Main {
         StringTokenizer st;
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
+        computers = new List[n + 1];
         visited = new boolean[n + 1];
-        arr = new int[n + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
+        for(int i = 1; i < n + 1; i++) {
+            computers[i] = new ArrayList<>();
+        }
+        for(int i = 0 ; i < m ; i++){
             st = new StringTokenizer(br.readLine()," ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = 1;
-            arr[b][a] = 1;
+            computers[a].add(b);
+            computers[b].add(a);
         }
-        dfs(1);
-        System.out.println(count-1);
+        bfs(1);
+        System.out.println(count - 1);
+
+
     }
-    public static void dfs(int i){
-        visited[i] = true;
-        count++;
-        for(int j = 1; j <= n; j++){
-            if(arr[i][j] == 1 && !visited[j]){
-                dfs(j);
+
+    public static void bfs(int i) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            if(!visited[cur]) {
+                visited[cur] = true;
+                count++;
+                for(int j = 0; j < computers[cur].size(); j++) {
+                    q.add((int)computers[cur].get(j));
+                }
             }
 
         }
     }
+
 
 
 }
