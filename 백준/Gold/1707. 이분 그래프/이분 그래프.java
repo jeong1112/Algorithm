2d1abeab0dc0a6d73058880dd581c7bb163cb315
이정연 <161396355+dlwjddus1112@users.noreply.github.com>
2025-03-04 15:55:13 +0900

    [Gold IV] Title: 이분 그래프, Time: 816 ms, Memory: 270460 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -10,47 +10,50 @@ public class Main {
     static boolean isBipartite;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        for (int i = 0; i < k; i++) {
+        int K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int v = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            graph = new ArrayList[v+1];
-            colors = new int[v+1];
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            graph = new ArrayList[V+1];
+            colors = new int[V+1];
             isBipartite = true;
-            for(int a = 0; a < v+1; a++) {
+            for(int a = 0; a < V+1; a++) {
                 graph[a] = new ArrayList<>();
             }
-            for(int j = 0; j < e; j++) {
+            for(int j = 0 ; j < E ; j++){
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 graph[a].add(b);
                 graph[b].add(a);
             }
-            for(int b = 1; b < v+1; b++) {
-                if(!isBipartite) {
+            for(int b = 1 ; b < V+1 ; b++){
+                if(!isBipartite){
                     break;
                 }
-                if(colors[b] == 0) {
+                if(colors[b] == 0){
                     dfs(b,1);
                 }
             }
             System.out.println(isBipartite ? "YES" : "NO");
         }
+
     }
+
     private static void dfs(int start, int color){
         colors[start] = color;
-        for(int v : graph[start]) {
-            if(colors[v] == color) {
+        for(int v : graph[start]){
+            if(colors[v] == color){
                 isBipartite = false;
                 return;
             }
             if(colors[v] == 0){
-                dfs(v,-color);
+                dfs(v, -color);
             }
         }
     }
+
 
 }
