56e5c47763d6436ab3b7348f1c9b1e8ee80eace0
이정연 <161396355+dlwjddus1112@users.noreply.github.com>
2025-03-05 17:09:14 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 121244 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,62 +6,70 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int m;
-    static int n;
-    static int[][] map;
-    static int[] dx = {-1, 1, 0 ,0};
-    static int[] dy = {0, 0, -1, 1};
-    static Queue<int[]> q = new LinkedList<>();
-    public static void main(String[] args) throws IOException {
+    static int[][] graph;
+    static boolean[][] visited;
+    static Queue<int[]> queue = new LinkedList<>();
+    static int[] dX = {-1,1,0,0};
+    static int[] dY = {0,0,-1,1};
+    static int n,m;
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        for(int i = 0; i < n; i++) {
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+        for(int i = 0 ; i < n ; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 1) q.add(new int[]{i, j});
-            }
-        }
-
-        System.out.println(bfs());
-    }
-    private static int bfs(){
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            for(int i = 0; i < 4 ; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if(map[nx][ny] == 0){
-                    map[nx][ny] = map[x][y] + 1;
-                    q.add(new int[]{nx, ny});
+            for(int j = 0 ; j < m ; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                if(graph[i][j] == 1){
+                    queue.add(new int[]{i,j});
                 }
             }
         }
-        int temp = Integer.MIN_VALUE;
+        System.out.println(bfs());
+
+    }
+
+    private static int bfs(){
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int x = cur[0];
+            int y = cur[1];
+            for(int i = 0 ; i < 4 ; i++){
+                int nX = x + dX[i];
+                int nY = y + dY[i];
+                if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
+                if(graph[nX][nY] == 0){
+                    graph[nX][nY] = graph[x][y] + 1;
+                    queue.add(new int[]{nX, nY});
+                }
+            }
+        }
         if(isZero()){
             return -1;
         }
-        else{
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(map[i][j] > temp){
-                        temp = map[i][j];
-                    }
+        int temp = Integer.MIN_VALUE;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(graph[i][j] > temp){
+                    temp = graph[i][j];
                 }
             }
         }
         return temp - 1;
     }
+
+
+
     private static boolean isZero(){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(map[i][j] == 0) return true;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < m ; j++){
+                if(graph[i][j] == 0){
+                    return true;
+                }
             }
         }
         return false;
