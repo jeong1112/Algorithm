87efe7679783845c620a1f3d424496ef98bd925a
이정연 <161396355+jeong1112@users.noreply.github.com>
2025-05-01 20:11:46 +0900

    [Gold V] Title: 암호 만들기, Time: 140 ms, Memory: 14948 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    private static int L,C;
+    private static char[] alphabets;
+    private static char[] result;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        alphabets = new char[C];
+        result = new char[L];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < C ;i++){
+            alphabets[i] = st.nextToken().charAt(0);
+        }
+        Arrays.sort(alphabets);
+        dfs(0,0);
+
+    }
+
+    private static void dfs(int depth, int start){
+        if(depth == L){
+            if(isValidChar(result)){
+                for (char c : result) {
+                    System.out.print(c);
+                }
+                System.out.println();
+            }
+            return;
+        }
+        for(int i = start; i < C ; i++){
+            result[depth] = alphabets[i];
+            dfs(depth + 1, i+1);
+        }
+    }
+
+    private static boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    private static boolean isValidChar(char[] arr){
+        int vowelCount = 0;
+        int consonantCount = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(isVowel(arr[i])){
+                vowelCount++;
+            }
+            else{
+                consonantCount++;
+            }
+        }
+        return vowelCount >= 1 && consonantCount >= 2;
+    }
+
+
+}
