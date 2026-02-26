af8382433947fcdd1beab74a9358c050eb887594
이정연 <161396355+jeong1112@users.noreply.github.com>
2026-02-26 17:40:36 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 44.51 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,36 +1,37 @@
-import java.util.*;
-
+// 숙련도가 난이도 이상이면 틀리지 않고 소요 시간만큼 사용
+// 숙련도보다 난이도가 높으면 diff - level번 틀린다. 틀릴 때마다 time_cur 시간 사용하고 '추가로' time_prev 사용해서 이전 퍼즐 풀어야 함. 이전 퍼즐은 틀리지 않는다.
+// diff-level, 즉 숙련도와 난이도의 차이만큼 틀리고 퍼즐을 풀면 time_cur만큼의 시간 사용하고 푼다. 
+// 제한 시간 내에 퍼즐을 해결하기 위한 숙련도의 최솟값. 
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int left = 1;
-        int right = 100000;
-        int size = diffs.length;
-        int answer = right;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            long temp = limit;
-
-            for (int i = 0; i < size; i++) {
-                if (diffs[i] <= mid) {
-                    temp -= times[i];
-                } else {
-                    int diff = diffs[i] - mid;
-                    int prevTime = (i == 0 ? 0 : times[i - 1]);
-                    temp -= (long)(times[i] + prevTime) * diff + times[i];
+        
+        int low = 1;
+        int high = 0;
+        for (int d : diffs) high = Math.max(high, d);
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            long time = times[0];
+ 
+            for(int i = 1; i < diffs.length; i++){
+                time += times[i];
+                
+                if(mid < diffs[i]){
+                    long tryCount = diffs[i] - mid;
+                    long tryTime = times[i] + times[i-1];
+                    time += tryCount * tryTime;
                 }
-
-                if (temp < 0) break;
             }
-
-            if (temp >= 0) {
-                answer = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1; 
+            
+            if(time <= limit){
+                high = mid - 1;
             }
+            else{
+                low = mid + 1;
+            }
+            
         }
-
-        return answer;
+        
+        return low;
     }
 }
