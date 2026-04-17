ad469bdcb23caa96527ebcd71dba06915de48045
이정연 <161396355+jeong1112@users.noreply.github.com>
2026-04-17 17:26:19 +0900

    [Gold V] Title: 물병, Time: 112 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int answer = 0;
+        while (Integer.bitCount(n) > k) {
+            n++;
+            answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
